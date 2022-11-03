cb10b83db515069ab3448e27997d9cd5a11ec343
Maksym Kot <redkotyara@yahoo.com>
2022-11-03 23:03:39 +0200

    Lab 4

--- DataEncryptionLab4_Kot_PC_21_MH/Form1.Designer.cs
+++ DataEncryptionLab4_Kot_PC_21_MH/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace DataEncryptionLab4_Kot_PC_21_MH
+﻿using System.Windows.Forms;
+
+namespace DataEncryptionLab4_Kot_PC_21_MH
 {
     partial class Form1
     {
@@ -29,12 +31,240 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_fileOUT = new System.Windows.Forms.Button();
+            this.button_fileIN = new System.Windows.Forms.Button();
+            this.textBox_fileOUT = new System.Windows.Forms.TextBox();
+            this.textBox_fileIN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.file_size = new System.Windows.Forms.Label();
+            this.button_KEYgenerator = new System.Windows.Forms.Button();
+            this.button_fileKEYsave = new System.Windows.Forms.Button();
+            this.button_fileKEYopen = new System.Windows.Forms.Button();
+            this.textBox_fileKEY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_time = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_fileOUT);
+            this.groupBox1.Controls.Add(this.button_fileIN);
+            this.groupBox1.Controls.Add(this.textBox_fileOUT);
+            this.groupBox1.Controls.Add(this.textBox_fileIN);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(47, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Файли";
+            // 
+            // button_fileOUT
+            // 
+            this.button_fileOUT.Location = new System.Drawing.Point(436, 69);
+            this.button_fileOUT.Name = "button_fileOUT";
+            this.button_fileOUT.Size = new System.Drawing.Size(111, 20);
+            this.button_fileOUT.TabIndex = 5;
+            this.button_fileOUT.Text = "- - -";
+            this.button_fileOUT.UseVisualStyleBackColor = true;
+            this.button_fileOUT.Click += new System.EventHandler(this.button_fileOUT_Click);
+            // 
+            // button_fileIN
+            // 
+            this.button_fileIN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_fileIN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_fileIN.Location = new System.Drawing.Point(436, 27);
+            this.button_fileIN.Name = "button_fileIN";
+            this.button_fileIN.Size = new System.Drawing.Size(111, 20);
+            this.button_fileIN.TabIndex = 4;
+            this.button_fileIN.Text = "- - -";
+            this.button_fileIN.UseVisualStyleBackColor = true;
+            this.button_fileIN.Click += new System.EventHandler(this.button_fileIN_Click);
+            // 
+            // textBox_fileOUT
+            // 
+            this.textBox_fileOUT.Location = new System.Drawing.Point(101, 69);
+            this.textBox_fileOUT.Name = "textBox_fileOUT";
+            this.textBox_fileOUT.Size = new System.Drawing.Size(305, 20);
+            this.textBox_fileOUT.TabIndex = 3;
+            // 
+            // textBox_fileIN
+            // 
+            this.textBox_fileIN.Location = new System.Drawing.Point(101, 27);
+            this.textBox_fileIN.Name = "textBox_fileIN";
+            this.textBox_fileIN.Size = new System.Drawing.Size(305, 20);
+            this.textBox_fileIN.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Зберегти у файл";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Відкритий файл:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.file_size);
+            this.groupBox2.Controls.Add(this.button_KEYgenerator);
+            this.groupBox2.Controls.Add(this.button_fileKEYsave);
+            this.groupBox2.Controls.Add(this.button_fileKEYopen);
+            this.groupBox2.Controls.Add(this.textBox_fileKEY);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(52, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(566, 130);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ключ";
+            // 
+            // file_size
+            // 
+            this.file_size.AutoSize = true;
+            this.file_size.Location = new System.Drawing.Point(32, 73);
+            this.file_size.Name = "file_size";
+            this.file_size.Size = new System.Drawing.Size(0, 13);
+            this.file_size.TabIndex = 5;
+            // 
+            // button_KEYgenerator
+            // 
+            this.button_KEYgenerator.BackgroundImage = global::DataEncryptionLab4_Kot_PC_21_MH.Resource1.key_icon;
+            this.button_KEYgenerator.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_KEYgenerator.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_KEYgenerator.Location = new System.Drawing.Point(203, 53);
+            this.button_KEYgenerator.Name = "button_KEYgenerator";
+            this.button_KEYgenerator.Size = new System.Drawing.Size(73, 71);
+            this.button_KEYgenerator.TabIndex = 4;
+            this.button_KEYgenerator.UseVisualStyleBackColor = true;
+            this.button_KEYgenerator.Click += new System.EventHandler(this.button_KEYgenerator_Click);
+            // 
+            // button_fileKEYsave
+            // 
+            this.button_fileKEYsave.BackgroundImage = global::DataEncryptionLab4_Kot_PC_21_MH.Resource1.save_file_disk_open_searsh_loading_clipboard_1513;
+            this.button_fileKEYsave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_fileKEYsave.Location = new System.Drawing.Point(436, 68);
+            this.button_fileKEYsave.Name = "button_fileKEYsave";
+            this.button_fileKEYsave.Size = new System.Drawing.Size(111, 62);
+            this.button_fileKEYsave.TabIndex = 3;
+            this.button_fileKEYsave.UseVisualStyleBackColor = true;
+            this.button_fileKEYsave.Click += new System.EventHandler(this.button_fileKEYsave_Click);
+            // 
+            // button_fileKEYopen
+            // 
+            this.button_fileKEYopen.BackgroundImage = global::DataEncryptionLab4_Kot_PC_21_MH.Resource1.open_file_40455;
+            this.button_fileKEYopen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_fileKEYopen.Location = new System.Drawing.Point(436, 12);
+            this.button_fileKEYopen.Name = "button_fileKEYopen";
+            this.button_fileKEYopen.Size = new System.Drawing.Size(106, 50);
+            this.button_fileKEYopen.TabIndex = 2;
+            this.button_fileKEYopen.UseVisualStyleBackColor = true;
+            this.button_fileKEYopen.Click += new System.EventHandler(this.button_fileKEYopen_Click);
+            // 
+            // textBox_fileKEY
+            // 
+            this.textBox_fileKEY.Location = new System.Drawing.Point(96, 32);
+            this.textBox_fileKEY.Name = "textBox_fileKEY";
+            this.textBox_fileKEY.Size = new System.Drawing.Size(310, 20);
+            this.textBox_fileKEY.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Файл з ключем";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(488, 319);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(111, 24);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "Старт";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Тривалість шифрування (мін:сек:мс):";
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Location = new System.Drawing.Point(328, 330);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(0, 13);
+            this.label_time.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
+        private System.Windows.Forms.Label file_size;
+
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_fileOUT;
+        private System.Windows.Forms.Button button_fileIN;
+        private System.Windows.Forms.TextBox textBox_fileOUT;
+        private System.Windows.Forms.TextBox textBox_fileIN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_KEYgenerator;
+        private System.Windows.Forms.Button button_fileKEYsave;
+        private System.Windows.Forms.Button button_fileKEYopen;
+        private System.Windows.Forms.TextBox textBox_fileKEY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- DataEncryptionLab4_Kot_PC_21_MH/Form1.cs
+++ DataEncryptionLab4_Kot_PC_21_MH/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace DataEncryptionLab4_Kot_PC_21_MH
@@ -15,6 +11,199 @@ namespace DataEncryptionLab4_Kot_PC_21_MH
         public Form1()
         {
             InitializeComponent();
+            toolTip1.SetToolTip(button_fileKEYsave, "Зберегти");
+            toolTip1.SetToolTip(button_fileKEYopen, "Відкрити");
+        }
+
+        private void button_fileIN_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog()
+            {
+                InitialDirectory = Directory.GetCurrentDirectory(),
+                Filter = "text files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_fileIN.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void button_fileOUT_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox_fileOUT.Text = saveFileDialog.FileName;
+            }
+        } 
+
+        private void button_fileKEYopen_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog()
+            {
+                InitialDirectory = Directory.GetCurrentDirectory(),
+                Filter = "text files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_fileKEY.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void button_fileKEYsave_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox_fileKEY.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void button_KEYgenerator_Click(object sender, EventArgs e)
+        {
+            var dialogResult = MessageBox.Show(
+                "Стоворити новий ключ?", 
+                "Увага",
+                MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                string fileINpath = textBox_fileIN.Text;
+                if (File.Exists(fileINpath))
+                {
+                    var fileKEYpath = textBox_fileKEY.Text;
+                    var dirKEYpath = Path.GetDirectoryName(fileKEYpath);
+                    if (Directory.Exists(dirKEYpath))
+                    {
+                        var lengthINfile = new FileInfo(fileINpath).Length;
+                        var arrKEY = new byte[lengthINfile];
+                        var rngCsp = new RNGCryptoServiceProvider();
+                        rngCsp.GetBytes(arrKEY);
+                        using (var fs = File.Create(fileKEYpath))
+                        {
+                            fs.Write(arrKEY, 0, arrKEY.Length);
+                            fs.Close();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Шлях до ключа не вказаний\nабо такий шлях не існує");
+                        textBox_fileKEY.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Вхідний файл не існує");
+                    button_fileIN.Focus();
+                }
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void mySaveToFileOUT(byte[] arrCipher, string fileOutPath)
+        {
+            using(var fs = File.Create(fileOutPath))
+            {
+                fs.Write(arrCipher, 0, arrCipher.Length);
+                fs.Close();
+            }
+
+            file_size.Text = $"Розмір ключа: \n{arrCipher.Length} байт";
+        }
+
+        private byte[] myCipherXOR(string fileINPath, string fileKEYPath)
+        {
+            var arrIN = File.ReadAllBytes(fileINPath);
+            var arrKEY = File.ReadAllBytes(fileKEYPath);
+            var lenIN = arrIN.Length;
+            var arrCipher = new byte[lenIN];
+
+            for (var i = 0; i < lenIN; i++)
+            {
+                var p = arrIN[i];
+                var k = arrKEY[i % lenIN];
+                
+                arrCipher[i] = (byte)(p ^ k);
+            }
+
+            return arrCipher;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            var fileINpath = textBox_fileIN.Text;
+            var fileOUTPath = textBox_fileOUT.Text;
+            var fileKEYpath = textBox_fileKEY.Text;
+
+            if (File.Exists(fileINpath))
+            {
+                var dirOutPath = Path.GetDirectoryName(fileOUTPath);
+                if (Directory.Exists(dirOutPath))
+                {
+                    if (File.Exists(fileKEYpath))
+                    {
+                        var lenINfile = new FileInfo(fileINpath).Length;
+                        var lenKeyFIle = new FileInfo(fileKEYpath).Length;
+
+                        if (lenINfile == lenKeyFIle || lenINfile >= lenKeyFIle)
+                        {
+                            var stopwatch = new Stopwatch();
+                            stopwatch.Start();
+
+                            var arrCipher = myCipherXOR(fileINpath, fileKEYpath);
+                            mySaveToFileOUT(arrCipher, fileOUTPath);
+
+                            stopwatch.Stop();
+                            label_time.Text = stopwatch.Elapsed.ToString(@"mm\:ss\.fff");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Розмір файлу ключа не співпадає\nз розміром або довжина файлу ключа більша");
+                            button_fileKEYopen.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Шлях до файлу ключа не вказаний\n або такий файл не існує");
+                        textBox_fileKEY.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Шлях до вихідного файлу не вказаний\n або такий шлях не існує");
+                    textBox_fileOUT.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вхідний файл не існує");
+                button_fileIN.Focus();
+            }
+
+            Cursor.Current = Cursors.Default;
         }
     }
 }
